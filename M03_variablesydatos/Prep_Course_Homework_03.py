709893eb798b312fddbae654082b6ac7a3e880cb
nayivis <115739913+nayivis@users.noreply.github.com>
2025-11-21 11:50:12 -0500

    Update Prep_Course_Homework_03.py

--- M03_variablesydatos/Prep_Course_Homework_03.py
+++ M03_variablesydatos/Prep_Course_Homework_03.py
@@ -7,13 +7,14 @@
 
 # In[7]:
 
-
+a = 12
+print(a)
 
 
 # 2) Imprimir el tipo de dato de la constante 8.5
 
 # In[3]:
-
+type(8.5)
 
 
 
@@ -21,6 +22,7 @@
 # 3) Imprimir el tipo de dato de la variable creada en el punto 1
 
 # In[8]:
+type(a)
 
 
 
@@ -29,6 +31,7 @@
 # 4) Crear una variable que contenga tu nombre
 
 # In[2]:
+mi_nombre='Nayivis'
 
 
 
@@ -36,6 +39,7 @@
 # 5) Crear una variable que contenga un número complejo
 
 # In[3]:
+num_complejo = 5 + 2i
 
 
 
@@ -44,6 +48,7 @@
 # 6) Mostrar el tipo de dato de la variable crada en el punto 5
 
 # In[4]:
+type(num_complejo)
 
 
 
@@ -60,7 +65,8 @@ pi = 3.1416
 # 8) Crear una variable que contenga el valor 'True' y otra que contenga el valor True. ¿Se trata de lo mismo?
 
 # In[3]:
-
+var1='True'
+var2=True
 
 
 
@@ -68,6 +74,7 @@ pi = 3.1416
 # 9) Imprimir el tipo de dato correspondientes a las variables creadas en el punto 8
 
 # In[5]:
+print('la variable 1 es de tipo : ,'type(var1),  'la variable 2 es de tipo: ' , type(var2))
 
 
 
@@ -77,13 +84,16 @@ pi = 3.1416
 
 # In[1]:
 
-
+var = 5 + 1,23
 
 
 
 # 11) Realizar una operación de suma de números complejos
 
 # In[2]:
+a = 2 + 3j
+b = 4 + 3j
+print ('la suma de los numeros complejos es ', a+b)
 
 
 
@@ -92,14 +102,16 @@ pi = 3.1416
 # 12) Realizar una operación de suma de un número real y otro complejo
 
 # In[4]:
-
+a = 2 + 3,4
+print (a)
 
 
 
 
 # 13) Realizar una operación de multiplicación
 
-# In[5]:
+# In[5]: 
+print( 3 * 4)
 
 
 
@@ -107,14 +119,17 @@ pi = 3.1416
 
 # 14) Mostrar el resultado de elevar 2 a la octava potencia
 
-# In[6]:
+# In[6]: 
+print(2**8)
 
 
 
 
 # 15) Obtener el cociente de la división de 27 entre 4 en una variable y luego mostrarla
 
-# In[8]:
+# In[8]: 
+a= 27 / 4
+print( a)
 
 
 
@@ -122,7 +137,8 @@ pi = 3.1416
 
 # 16) De la división anterior solamente mostrar la parte entera
 
-# In[9]:
+# In[9]: 
+print(27 // 4)
 
 
 
@@ -131,6 +147,7 @@ pi = 3.1416
 # 17) De la división de 27 entre 4 mostrar solamente el resto
 
 # In[1]:
+print(27 % 4)
 
 
 
@@ -139,6 +156,7 @@ pi = 3.1416
 # 18) Utilizando como operandos el número 4 y los resultados obtenidos en los puntos 16 y 17. Obtener 27 como resultado
 
 # In[2]:
+print( 6 * 4 + 3)
 
 
 
@@ -147,6 +165,9 @@ pi = 3.1416
 # 19) Utilizar el operador "+" en una operación donde intervengan solo variables alfanuméricas
 
 # In[3]:
+a = 'Santa '
+b = 'Marta '
+print ( a + b )
 
 
 
@@ -155,7 +176,8 @@ pi = 3.1416
 # 20) Evaluar si "2" es igual a 2. ¿Por qué ocurre eso?
 
 # In[4]:
-
+'2' == 2
+# son diferentes porque na es de tipo entero y la otra de tipo string
 
 
 
@@ -163,7 +185,7 @@ pi = 3.1416
 # 21) Utilizar las funciones de cambio de tipo de dato, para que la validación del punto 20 resulte verdadera
 
 # In[11]:
-
+2== int('2')
 
 
 
@@ -171,7 +193,7 @@ pi = 3.1416
 # 22) ¿Por qué arroja error el siguiente cambio de tipo de datos? a = float('3,8')
 
 # In[12]:
-
+#arroja error porque cadena tiene un formato no valido para formato float, se le debe quitar la coma y ponerle .
 
 
 
@@ -179,6 +201,9 @@ pi = 3.1416
 # 23) Crear una variable con el valor 3, y utilizar el operador '-=' para modificar su contenido y que de como resultado 2.
 
 # In[15]:
+a = 3
+a -= 1
+print (a)
 
 
 
@@ -188,15 +213,18 @@ pi = 3.1416
 
 # In[29]:
 
-
+"""La operación 1<<2 da como resultado 4 porque el operador << es un desplazamiento a la izquierda en lenguaje binario, lo que significa que toma el número binario de 1 y desplaza sus bits dos posiciones a la izquierda. El número binario de 1 es 01 (si imaginamos que es de 2 bits), y al desplazarlo dos posiciones a la izquierda, el resultado binario es 100, que en decimal es 4.
+Paso 1: Representa el número 1 en binario. Para simplificar, pensemos en 1 como 01.
+Paso 2: Aplica el desplazamiento a la izquierda <<2. Esto significa que mueves todos los bits del número dos posiciones hacia la izquierda. Las posiciones que quedan vacías a la derecha se rellenan con 0.01 se convierte en 100 (dos posiciones a la izquierda).
+Paso 3: Convierte el resultado binario 100 a decimal.El valor posicional de cada bit es: \((1\times 2^{2})+(0\times 2^{1})+(0\times 2^{0})=4+0+0=4\)"""
 
 
 
 # 25) Realizar la operación 2 + '2' ¿Por qué no está permitido? ¿Si los dos operandos serían del mismo tipo, siempre arrojaría el mismo resultado?
 
 # In[23]:
-
-
+# no está permitido porque las variables son de difrentes tipos
+# si los las dos variables fueran del mismo tipo no arrojaría ese resultado
 
 
 
@@ -204,6 +232,7 @@ pi = 3.1416
 # 26) Realizar una operación válida entre valores de tipo entero y string
 
 # In[30]:
+int(2) + int ('2')
 
 
 
